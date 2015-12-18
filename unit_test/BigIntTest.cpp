a8e74f2b9fbf84cd269f2efd5fbccab8f90ebdbf
Michael Weidner <michael.weidner@motorolasolutions.com>
2015-12-18 11:12:34 -0500

    baseline commit

---
+++ unit_test/BigIntTest.cpp
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ *
+ *             COPYRIGHT 2015 MOTOROLA SOLUTIONS INC. ALL RIGHTS RESERVED.
+ *                    MOTOROLA SOLUTIONS CONFIDENTIAL RESTRICTED
+ *                            TEMPLATE VERSION R01.03
+ *
+ ********************************************************************************
+ *
+ *  FILE NAME : BigIntTest.cpp
+ *
+ *-------------------------------- PURPOSE --------------------------------------
+ *
+ *
+ *--------------------------- DEPENDENCY COMMENTS -------------------------------
+ *
+ *
+ *-------------------------- PROJECT SPECIFIC DATA ------------------------------
+ *
+ *
+ *----------------------------- REVISION HISTORY --------------------------------
+ *
+ * Date      Core ID   Tracking#      Description
+ * --------  --------  -------------  -------------------------------------------
+ *           vagrant                  Initial Creation
+ *
+ *******************************************************************************/
+
+/*--------------------------- HEADER FILE INCLUDES ----------------------------*/
+//#include "BigIntTest.h"
+#include "CppUTest/TestHarness.h"
+
+TEST_GROUP(FirstTestGroup)
+{
+};
+
+TEST(FirstTestGroup, FirstTest)
+{
+   CHECK_EQUAL(0,0);
+}
---
+++ unit_test/TestRunner.cpp
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ *
+ *             COPYRIGHT 2015 MOTOROLA SOLUTIONS INC. ALL RIGHTS RESERVED.
+ *                    MOTOROLA SOLUTIONS CONFIDENTIAL RESTRICTED
+ *                            TEMPLATE VERSION R01.03
+ *
+ ********************************************************************************
+ *
+ *  FILE NAME : TestRunner.cpp
+ *
+ *-------------------------------- PURPOSE --------------------------------------
+ *
+ *
+ *--------------------------- DEPENDENCY COMMENTS -------------------------------
+ *
+ *
+ *-------------------------- PROJECT SPECIFIC DATA ------------------------------
+ *
+ *
+ *----------------------------- REVISION HISTORY --------------------------------
+ *
+ * Date      Core ID   Tracking#      Description
+ * --------  --------  -------------  -------------------------------------------
+ *           vagrant                  Initial Creation
+ *
+ *******************************************************************************/
+
+/*--------------------------- HEADER FILE INCLUDES ----------------------------*/
+#include "TestRunner.h"
+
+#include "CppUTest/CommandLineTestRunner.h"
+
+int main(int ac, char** av)
+{
+    return CommandLineTestRunner::RunAllTests(ac, av);
+}
